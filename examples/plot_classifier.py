6efbc293d91d503371dcc524a3097ce86c81584c
Yudong He <52923246+ydcnanhe@users.noreply.github.com>
2025-09-23 20:54:13 +0800

    delete unnecessary files

---
+++ examples/plot_classifier.py
@@ -0,0 +1,41 @@
+"""
+============================
+Plotting Template Classifier
+============================
+
+An example plot of :class:`sklekmeans.template.TemplateClassifier`
+"""
+
+# %%
+# Train our classifier on very simple dataset
+from sklekmeans import TemplateClassifier
+
+X = [[0, 0], [1, 1]]
+y = [0, 1]
+clf = TemplateClassifier().fit(X, y)
+
+# %%
+# Create a test dataset
+import numpy as np
+
+rng = np.random.RandomState(13)
+X_test = rng.rand(500, 2)
+
+# %%
+# Use scikit-learn to display the decision boundary
+from sklearn.inspection import DecisionBoundaryDisplay
+
+disp = DecisionBoundaryDisplay.from_estimator(clf, X_test)
+disp.ax_.scatter(
+    X_test[:, 0],
+    X_test[:, 1],
+    c=clf.predict(X_test),
+    s=20,
+    edgecolors="k",
+    linewidths=0.5,
+)
+disp.ax_.set(
+    xlabel="Feature 1",
+    ylabel="Feature 2",
+    title="Template Classifier Decision Boundary",
+)
